001918b1ab7617994547fc2a36bf36c72aa2b1aa
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-04 16:10:30 -0700

    Update Albums at regular interval, ensure only one application instance does it.

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -3,9 +3,12 @@ package org.superbiz.moviefun.albums;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAsync
@@ -13,26 +16,45 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class AlbumsUpdateScheduler {
 
     private static final long SECONDS = 1000;
-    private static final long MINUTES = 60 * SECONDS;
 
+    private final JdbcTemplate jdbcTemplate;
     private final AlbumsUpdater albumsUpdater;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    public AlbumsUpdateScheduler(DataSource dataSource, AlbumsUpdater albumsUpdater) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.albumsUpdater = albumsUpdater;
     }
 
 
-    @Scheduled(initialDelay = 15 * SECONDS, fixedRate = 2 * MINUTES)
+    @Scheduled(initialDelay = 5 * SECONDS, fixedRate = 15 * SECONDS)
     public void run() {
         try {
-            logger.debug("Starting albums update");
-            albumsUpdater.update();
+            logger.debug("Checking for albums task to start");
 
-            logger.debug("Finished albums update");
+            if (startAlbumSchedulerTask()) {
+                logger.debug("Starting albums update");
+
+                albumsUpdater.update();
+
+                logger.debug("Finished albums update");
+            } else {
+                logger.debug("Nothing to start");
+            }
 
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
+    }
+
+    private boolean startAlbumSchedulerTask() {
+        int updatedRows = jdbcTemplate.update(
+            "UPDATE album_scheduler_task" +
+                " SET started_at = now()" +
+                " WHERE started_at IS NULL" +
+                " OR started_at < date_sub(now(), INTERVAL 2 MINUTE)"
+        );
+
+        return updatedRows > 0;
     }
 }
