d1baaffcafb9a7b277b63042294022bb8745f8a4
Trevor Whitney <twhitney@pivotal.io>
2017-01-10 12:33:39 -0700

    fixed smoke test

--- src/test/java/org/superbiz/moviefun/SmokeTest.java
+++ src/test/java/org/superbiz/moviefun/SmokeTest.java
@@ -16,7 +16,7 @@ public class SmokeTest {
 
         assertThat(homePage, containsString("Please select one of the following links:"));
 
-        String setupPage = restTemplate.getForObject(url("/setup.jsp"), String.class);
+        String setupPage = restTemplate.getForObject(url("/setup"), String.class);
 
         assertThat(setupPage, containsString("Wedding Crashers"));
         assertThat(setupPage, containsString("Starsky & Hutch"));
@@ -31,7 +31,7 @@ public class SmokeTest {
     }
 
     private String url(String path) {
-        String baseUrl = "http://localhost:8080/moviefun";
+        String baseUrl = "http://localhost:8080";
         String envUrl = System.getenv("MOVIE_FUN_URL");
 
         if (envUrl != null && !envUrl.isEmpty()) {
